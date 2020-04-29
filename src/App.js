07a5c623dd37350233c2a591dd11d0657b15770a
Giuliano Piovezan <giulianopiovezan@gmail.com>
2020-04-28 21:31:58 -0300

    Resolucao do desafio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    api.get('repositories')
+       .then(res => setRepositories(res.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post('repositories', { title });
+    setRepositories(prevState => [ ...prevState, data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories([ ...repositories.filter(repository => repository.id !== id) ]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repository => (
+            <li key={repository.id}>
+              { repository.title }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
-
+      <input type="text" placeholder="Repositorio" value={title} onChange={({ target }) => setTitle(target.value)} />
+      <input type="text" placeholder="URL" value={url} onChange={({ target }) => setUrl(target.value)} />
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
